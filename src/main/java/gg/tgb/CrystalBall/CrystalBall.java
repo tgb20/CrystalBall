78e00b56fb32662eaa851749de3a48bba723005f
Jacob Bashista <tgb.bashista@gmail.com>
2020-11-29 16:49:13 -0500

    Blocks Endpoint Test

--- src/main/java/gg/tgb/CrystalBall/CrystalBall.java
+++ src/main/java/gg/tgb/CrystalBall/CrystalBall.java
@@ -1,24 +1,97 @@
 package gg.tgb.CrystalBall;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import io.javalin.Javalin;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-public class CrystalBall extends JavaPlugin {
+import java.util.ArrayList;
+
+public class CrystalBall extends JavaPlugin implements Listener {
+
+
+    private int searchRadius = 10;
+
+    private Block[] blockList;
+    private Location playerPos;
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
         System.out.println("Crystal Ball is Starting");
-
 
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(Javalin.class.getClassLoader());
-        Javalin app = Javalin.create().start(8001);
+        Javalin app = Javalin.create().start(25599);
         app.config.addStaticFiles("/public");
+        app.get("/blocks", ctx -> {
+
+
+            JSONArray blocksArray = new JSONArray();
+
+            for(int i = 0; i < blockList.length; i++) {
+                Block b = blockList[i];
+
+                String type = b.getType().name();
+                int x = (int) playerPos.getX() - b.getX();
+                int y = (int) playerPos.getY() - b.getY();
+                int z = (int) playerPos.getZ() - b.getZ();
+
+                JSONObject jsonBlock = new JSONObject();
+
+                jsonBlock.put("type", type);
+                jsonBlock.put("x", x);
+                jsonBlock.put("y", y);
+                jsonBlock.put("z", z);
+
+                blocksArray.add(jsonBlock);
+            }
+
+            ctx.json(blocksArray);
+        });
         Thread.currentThread().setContextClassLoader(classLoader);
     }
 
     @Override
     public void onDisable() {
         System.out.println("Crystal Ball is Stopping");
+    }
+
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+
+        if(event.getPlayer().getName().equalsIgnoreCase("tgb0")) {
+
+            playerPos = event.getPlayer().getLocation();
+
+            World pWorld = playerPos.getWorld();
+            double pX = playerPos.getX();
+            double pY = playerPos.getY();
+            double pZ = playerPos.getZ();
+
+            ArrayList<Block> blocks = new ArrayList<Block>();
+
+            for(double x = pX-searchRadius; x < pX+searchRadius; x++) {
+                for(double y = pY-searchRadius; y < pY+searchRadius; y++) {
+                    for(double z = pZ-searchRadius; z < pZ+searchRadius; z++) {
+                        Block b = pWorld.getBlockAt(new Location(pWorld, x, y, z));
+
+                        if(!b.getType().equals(Material.AIR)) {
+                            blocks.add(b);
+                        }
+                    }
+                }
+            }
+            blockList = new Block[blocks.size()];
+            blockList = blocks.toArray(blockList);
+        }
     }
 }
--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -30,53 +30,9 @@ window.onload = (() => {
         new THREE.MeshBasicMaterial({ map: texture5 }) // SIDE
     ];
 
-    const cube1 = new THREE.Mesh(geometry, materials);
-    const cube2 = new THREE.Mesh(geometry, materials);
-    const cube3 = new THREE.Mesh(geometry, materials);
-    const cube4 = new THREE.Mesh(geometry, materials);
-    const cube5 = new THREE.Mesh(geometry, materials);
-    const cube6 = new THREE.Mesh(geometry, materials);
-    const cube7 = new THREE.Mesh(geometry, materials);
-    const cube8 = new THREE.Mesh(geometry, materials);
-    const cube9 = new THREE.Mesh(geometry, materials);
-    scene.add(cube1);
-    cube1.position.set(0, 0, 0);
-    scene.add(cube2);
-    cube2.position.set(1, 0, 0);
-    scene.add(cube3);
-    cube3.position.set(-1, 0, 0);
-    scene.add(cube4);
-    cube4.position.set(0, 0, -1);
-    scene.add(cube5);
-    cube5.position.set(0, 0, 1);
-    scene.add(cube6);
-    cube6.position.set(1, 0, 1);
-    scene.add(cube7);
-    cube7.position.set(1, 0, -1);
-    scene.add(cube8);
-    cube8.position.set(-1, 0, -1);
-    scene.add(cube9);
-    cube9.position.set(-1, 0, 1);
-
-    const log0 = textureLoader.load('textures/oak_log.png');
-    const log1 = textureLoader.load('textures/oak_log.png');
-    const log2 = textureLoader.load('textures/oak_log_top.png');
-    const log3 = textureLoader.load('textures/oak_log_top.png');
-    const log4 = textureLoader.load('textures/oak_log.png');
-    const log5 = textureLoader.load('textures/oak_log.png');
-
-    const logMaterials = [
-        new THREE.MeshBasicMaterial({ map: log0 }), // SIDE
-        new THREE.MeshBasicMaterial({ map: log1 }), // SIDE
-        new THREE.MeshBasicMaterial({ map: log2 }), // TOP
-        new THREE.MeshBasicMaterial({ map: log3 }), // BOTTOM
-        new THREE.MeshBasicMaterial({ map: log4 }), // SIDE
-        new THREE.MeshBasicMaterial({ map: log5 }) // SIDE
-    ];
-
-    const cube10 = new THREE.Mesh(geometry, logMaterials);
-    scene.add(cube10);
-    cube10.position.set(0, 1, 0);
+    const cube = new THREE.Mesh(geometry, materials);
+    cube.position.set(0, 0, 0);
+    scene.add(cube);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
